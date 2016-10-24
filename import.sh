329240aaf74da61a7dc81bf68a88c0ba7360c730
Igor Avdeev <lislon@mail.ru>
2016-10-24 17:45:04 +0300

    Add logging to waiting script.

--- import.sh
+++ import.sh
@@ -2,4 +2,9 @@
 # description: Loads a dump to ttrss image (should be running)
 #       usage: import.sh < dump.sql
 
+if [ -t 0 ]; then
+    echo "Usage $0 < dump.sql"
+    exit 1
+fi
+
 docker exec -i ttrss_db_1 mysql -uttrss -pttrss ttrss
--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -2,13 +2,19 @@
 #      author: lislon
 # description: Waits for mysql and then exit with 0 (success) or 1 (code)
 
-RETRY=60
+RETRY=10
 DB_HOST=db
 DB_PORT=3306
 
-while ! $(nc -z $DB_HOST $DB_PORT) && (( $RETRY > 1 )); do sleep 1; RETRY=$(($RETRY-1)); done
+echo "Waiting for mysql on $DB_HOST:$DB_PORT..."
+while ! $(nc -z $DB_HOST $DB_PORT) && (( $RETRY > 1 )); do
+    sleep 1;
+    ((RETRY--))
+done
 
 if (( $RETRY <= 1 )) ; then
     echo "Can't access db at host $DB_HOST port $DB_PORT" >&2
     exit 1
 fi
+
+echo "mysql is running"
